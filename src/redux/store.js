fc74985693bf7fe870b8182ee103353db84d3e06
Oleksii-Senchenko <106326629+Oleksii-Senchenko@users.noreply.github.com>
2023-09-11 16:27:14 +0300

    added persistor

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,17 +1,36 @@
 import { configureStore } from '@reduxjs/toolkit';
-// import contactsSlice from './contactsSlice';
 import singUpSlice from './auth/singUpSlice';
 import contactsSlice from './contacts/contactsSlice';
 import { filterSlice } from './contacts/filterSlice';
-// import { filterSlice } from './filterSlice';
-// import { registerUser } from './operations';
+import storage from 'redux-persist/lib/storage'
+import {
+    persistStore, persistReducer, FLUSH,
+    REHYDRATE,
+    PAUSE,
+    PERSIST,
+    PURGE,
+    REGISTER
+} from 'redux-persist';
 
+
+const persistConfig = {
+    key: 'token',
+    storage,
+    whitelist: ['token'],
+}
 
 
 export const store = configureStore({
     reducer: {
-        auth: singUpSlice.reducer,
+        auth: persistReducer(persistConfig, singUpSlice.reducer),
         contacts: contactsSlice.reducer,
-        filter: filterSlice.reducer
-    }
+        filter: filterSlice.reducer,
+    },
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware({
+            serializableCheck: {
+                ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+            },
+        }),
 });
+export const persistor = persistStore(store)
